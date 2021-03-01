4b2b664b1302c1b2209a67e525e6dcfab7308fc3
Andrea <git@andreaemmanuele.it>
2021-03-01 16:00:16 +0100

    Exercise completed

--- assets/js/ticket-price.js
+++ assets/js/ticket-price.js
@@ -1,29 +1,34 @@
-let km;
-let age;
+let km = document.getElementById("km");
+let age = document.getElementById("age");
 const priceKm = 0.21;
 
 function calc() {
-    let price;
+    if (km.value != 0 && age.value != 0) {
+        let price;
 
-    km = document.getElementById("km");
-    age = document.getElementById("age");
+        price = km.value * priceKm;
 
-    price = km.value * priceKm;
+        if (age.value < 18) {
+            price -= (price * 20 / 100);
+            price = (Math.round(price * 100) / 100).toFixed(2);
+            document.getElementById("price").innerHTML = price + "&euro;";
+        } else if (age.value > 65) {
+            price -= (price * 40 / 100);
+            price = (Math.round(price * 100) / 100).toFixed(2);
+            document.getElementById("price").innerHTML = price + "&euro;";
+        } else {
+            price = (Math.round(price * 100) / 100).toFixed(2);
+            document.getElementById("price").innerHTML = price + "&euro;";
+        }
 
-    if (age.value < 18) {
-        price -= (price * 20 / 100);
-        price = (Math.round(price * 100) / 100).toFixed(2);
-        document.getElementById("price").innerHTML = price + "&euro;";
-    }
-    else if (age.value > 65) {
-        price -= (price * 40 / 100);
-        price = (Math.round(price * 100) / 100).toFixed(2);
-        document.getElementById("price").innerHTML = price + "&euro;";
+        document.getElementById("line").style.display = "block";
+        document.getElementById("price").style.display = "block";
+        document.getElementById("error").style.display = "none";
     }
     else {
-        price = (Math.round(price * 100) / 100).toFixed(2);
-        document.getElementById("price").innerHTML = price + "&euro;";
+        document.getElementById("line").style.display = "block";
+        document.getElementById("price").style.display = "none";
+        document.getElementById("error").style.display = "block";
+        document.getElementById("error").innerHTML = "Uno o pi&ugrave; campi sono vuoti!";
     }
-
-    document.getElementById("price").style.display = "block";
 }
